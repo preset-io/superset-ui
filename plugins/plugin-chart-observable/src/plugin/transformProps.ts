bb9c7c305ebae8b1324a64770db3f880d8ade450
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2020-05-28 10:25:49 -0700

    fix Mr

--- plugins/plugin-chart-observable/src/plugin/transformProps.ts
+++ plugins/plugin-chart-observable/src/plugin/transformProps.ts
@@ -4,6 +4,7 @@ import { ObservableFormData } from '../types';
 
 export default function transformProps(chartProps: ChartProps): ObservableProps {
   const { width, height, formData, queryData } = chartProps;
+<<<<<<< Updated upstream
   const {
     observableUrl,
     displayedCells,
@@ -11,14 +12,24 @@ export default function transformProps(chartProps: ChartProps): ObservableProps 
     showDebug,
   } = formData as ObservableFormData;
 
+=======
+  const { observableUrl, displayedCells, showDebug } = formData as ObservableFormData;
+  console.log('displayedCells', displayedCells)
+>>>>>>> Stashed changes
   // console.warn('formData!!!!!!!!!!!', formData);
-
+  console.log('------------------')
+  console.log('------------------')
   return {
     data: queryData.data,
     height,
+<<<<<<< Updated upstream
     observableUrl: observableUrl || 'no workbook url selected',
     displayedCells: displayedCells || [],
     dataInjectionCell: dataInjectionCell || '',
+=======
+    observableUrl: observableUrl || 'nada',
+    displayedCells,
+>>>>>>> Stashed changes
     showDebug: showDebug || false,
     width,
   };
